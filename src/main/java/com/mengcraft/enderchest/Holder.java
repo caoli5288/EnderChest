8da2fb2792aa7246e75973e85c8580a067a785ad
caoli5288 <caoli5288@gmail.com>
2015-08-15 23:46:42 +0800

    Update to 0.2-SNAPSHOT

---
+++ src/main/java/com/mengcraft/enderchest/Holder.java
@@ -0,0 +1,14 @@
+package com.mengcraft.enderchest;
+
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+public class Holder implements InventoryHolder {
+
+	@Override
+	public Inventory getInventory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
